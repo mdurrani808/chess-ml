1a8f336867ab2c28f5284dd2e11e3a6fcc253074
Pranav Shah <pshah115@terpmail.umd.edu>
2024-04-12 17:43:01 -0400

    Added requirements.txt and evaluations.csv

--- data_split.py
+++ data_split.py
@@ -2,10 +2,12 @@ import chess.pgn
 import pandas as pd
 import glob
 
-
+pgnFilePath =  r"C:\Users\mdurr\Code\cmsc320\final\data\*.pgn"
+# pgnFilePath = r"C:\Users\pranavshah\Code\320\cmsc320_final_project\data\*.pgn"
+outputFilePath = "./evaluations.csv"
 totalGameCount = 0
 totalEvalGameCount = 0
-fileList = glob.glob(r"C:\Users\mdurr\Code\cmsc320\final\data\*.pgn") # modify this line for the directory that has all the pgn files 
+fileList = glob.glob(pgnFilePath) # modify this line for the directory that has all the pgn files 
 rows_list = [] # list of dictionaries for each game 
 for file in fileList:
     pgn = open(fileList[0])
@@ -21,7 +23,9 @@ for file in fileList:
             h = game.headers
             # adding a dictionary is faster than appending to a dataframe 
             rows_list.append({"UTCDate": h["UTCDate"], 'Result':h['Result'], 'WhiteElo':h['WhiteElo'], 'BlackElo':h['BlackElo'], "Opening": "0", "Something":"0", "Termination": h["Termination"], "Variations":str(variations[0]), 'WhiteRatingDiff':h['WhiteRatingDiff'], 'BlackRatingDiff':h["BlackRatingDiff"]})
+        # Iterator reading next game
         game = chess.pgn.read_game(pgn)
 
-df = pd.DataFrame(rows_list) # add everything to the dataframe              
+df = pd.DataFrame(rows_list) # add everything to the dataframe 
+df.to_csv(outputFilePath)             
 
