11764a2df0ce5642169d77313fda08c771b5ee9e
Mohammad Durrani <mdurrani808@gmail.com>
2024-04-13 16:31:30 -0400

    updated dictionary

--- data_split.py
+++ data_split.py
@@ -1,8 +1,9 @@
 import chess.pgn
 import pandas as pd
 import glob
+import numpy as np
 
-pgnFilePath =  r"C:\Users\mdurr\Code\cmsc320\final\data\*.pgn"
+pgnFilePath =  r"\\wsl.localhost\Ubuntu\home\mdurrani\lichess_db_standard_rated_2024-03.pgn"
 # pgnFilePath = r"C:\Users\pranavshah\Code\320\cmsc320_final_project\data\*.pgn"
 outputFilePath = "./evaluations.csv"
 totalGameCount = 0
@@ -15,14 +16,28 @@ for file in fileList:
     while game != None:
         totalGameCount += 1
         variations = game.variations # list of either the eval or clock score
-        if(totalGameCount % 100 == 0): # print every 2000 games
+        if(totalGameCount % 2000 == 0): # print every 2000 games
             print("Total Game Count: ", totalGameCount)
             print("Eval Game Count: ", totalEvalGameCount)
         if(len(variations) > 0 and 'eval' in variations[0].comment): # if this game was evaluated by a computer, add it
             totalEvalGameCount+=1
             h = game.headers
+                
             # adding a dictionary is faster than appending to a dataframe 
-            rows_list.append({"UTCDate": h["UTCDate"], "UTCTime": h["UTCTime"], 'WhiteElo':h['WhiteElo'], 'BlackElo':h['BlackElo'], "Opening": h["Opening"], "ECO":h["ECO"],'Result':h['Result'], "Termination": h["Termination"], "Variations":str(variations[0]), 'WhiteRatingDiff':h['WhiteRatingDiff'], 'BlackRatingDiff':h["BlackRatingDiff"]})
+            rows_list.append({
+                "UTCDate": h.get("UTCDate", np.NaN),
+                "UTCTime": h.get("UTCTime", np.NaN),
+                'WhiteElo': h.get('WhiteElo', np.NaN),
+                'BlackElo': h.get('BlackElo', np.NaN),
+                "Opening": h.get("Opening", np.NaN),
+                "ECO": h.get("ECO", np.NaN),
+                'Result': h.get('Result', np.NaN),
+                "Termination": h.get("Termination", np.NaN),
+                "Variations": str(variations[0]) if variations else np.NaN,
+                'WhiteRatingDiff': h.get('WhiteRatingDiff', np.NaN),
+                'BlackRatingDiff': h.get("BlackRatingDiff", np.NaN)
+            })
+
         # Iterator reading next game
         game = chess.pgn.read_game(pgn)
 
